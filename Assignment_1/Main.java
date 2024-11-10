d78ab263578db787a73842c4da0a16d8404d695f
sccjrd <saccof@usi.ch>
2024-11-10 22:52:09 +0100

    fixed analysis

--- Assignment_1/Main.java
+++ Assignment_1/Main.java
@@ -65,11 +65,11 @@ import java.io.IOException;
             if(type==String.class){
                 switch (i) {
                     case 1:
-                        return (T[])orderedStringArray(i);
+                        return (T[])orderedStringArray(size);
                     case 2:
-                        return (T[])reverseStringArray(i);
+                        return (T[])reverseStringArray(size);
                     case 3:
-                        return (T[])randomStringArray(i);
+                        return (T[])randomStringArray(size);
                     default:
                     throw new AssertionError("Undefined array  type");
                 }
@@ -109,41 +109,33 @@ import java.io.IOException;
                     throw new AssertionError("Undefined sorting algorithm");
             }
         }
-
-        public static <T extends Comparable<T>> Sorter<T> chooseSorting(Integer sortingAlg, Class<T> type){
-            Sorter<T> sorting;
-            switch (sortingAlg) {
-                case 1:
-                    return sorting=new BubbleSortUntilNoChange<>();
-                case 2:
-                    return sorting=new BubbleSortWhileNeeded<>();
-                case 3:
-                    return sorting=new QuickSortGPT<>();
-                case 4:
-                    return sorting=new SelectionSortGPT<>();
-                default:
-                    throw new AssertionError("Undefined sorting algorithm");
-            }
-        }
         
         public static void main(String[] args){     
             try (FileWriter writer = new FileWriter("ResultsInteger.csv", true)) {
                 Integer[] sizes = {20, 400, 10000};
-                for(Integer size:sizes){
-                    for(Integer t=1;t<=3;t++){
-                        for(Integer s=1;s<=4;s++){
-                            for(Integer iterations=0;iterations<100;iterations++){
-                            var sortName="bubblesort no change";
-                            var arrayName="ordered";
-                            if(s==2)sortName="bubblesort while needed";
-                            if(s==3)sortName="quicksortgpt";
-                            if(s==4)sortName="selectionsortgpt";
-                            if(t==2)arrayName="inverse ordered";
-                            if(t==3)arrayName="random";
-                            long startTime = System.nanoTime();
-                            chooseSorting(s, Integer.class).sort(chooseArray(Integer.class,t,size));
-                            long endTime = System.nanoTime();  
-                            writer.write(size + "," + sortName + "," + arrayName + "," + (endTime - startTime) + "\n");
+                int i=0;
+                writer.write("Iteration, ArraySize,Algorithm,Type,Time(ns)\n");
+                for(Integer size:sizes){                    
+                    for(Integer s=1;s<=4;s++){
+                        for(Integer iteration=0;iteration<100;iteration++){
+                            for(Integer t=1;t<=3;t++){
+                                var sortName="bubblesort no change";
+                                var arrayName="ordered";
+                                if(s==2)sortName="bubblesort while needed";
+                                if(s==3)sortName="quicksortgpt";
+                                if(s==4)sortName="selectionsortgpt";
+                                if(t==2)arrayName="inverse ordered";
+                                if(t==3)arrayName="random";
+
+                                Sorter<Integer> sorting=chooseSortingInteger(s);
+                                Integer [] array = chooseArray(Integer.class,t,size);
+
+                                long startTime = System.nanoTime();
+                                sorting.sort(array);
+                                long endTime = System.nanoTime();
+
+                                writer.write(i + "," + size + "," + sortName + "," + arrayName + "," + (endTime - startTime) + "\n");
+                                i++;
                             }
                         }
                     }
@@ -153,10 +145,12 @@ import java.io.IOException;
             }
             try (FileWriter writer = new FileWriter("ResultsString.csv", true)) {
                 Integer[] sizes = {20, 400, 10000};
-                for(Integer i:sizes){
-                    for(Integer j=1;j<=3;j++){
-                        for(Integer s=1;s<=4;s++){
-                            for(Integer iterations=0;iterations<100;iterations++){
+                int i=0;
+                writer.write("Iteration, ArraySize,Algorithm,Type,Time(ns)\n");
+                for(Integer size:sizes){
+                    for(Integer s=1;s<=4;s++){
+                            for(Integer iteration=0;iteration<100;iteration++){
+                                for(Integer j=1;j<=3;j++){
                                 var sortName="bubblesort no change";
                                 var arrayName="ordered";
                                 if(s==2)sortName="bubblesort while needed";
@@ -164,10 +158,16 @@ import java.io.IOException;
                                 if(s==4)sortName="selectionsortgpt";
                                 if(j==2)arrayName="inverse ordered";
                                 if(j==3)arrayName="random";
+
+                                Sorter<String> sorting=chooseSortingString(s);
+                                String [] array = chooseArray(String.class,j,size);
+                                
                                 long startTime = System.nanoTime();
-                                chooseSortingString(s).sort(chooseArray(String.class,j,i));
+                                sorting.sort(array);
                                 long endTime = System.nanoTime();  
-                                writer.write(i + "," + sortName + "," + arrayName + "," + (endTime - startTime) + "\n");
+
+                                writer.write(i + "," + size + "," + sortName + "," + arrayName + "," + (endTime - startTime) + "\n");
+                                i++;
                             }
                         }
                     }
