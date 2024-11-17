33514f0db5d4f296b9afe9354c52ec29721f311f
Cesco <saccof@usi.ch>
2024-11-17 20:00:33 +0100

    frontend

---
+++ Assignment_2/survey-app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container py-8">
+    <h1 class="text-2xl font-bold mb-4">Survey App</h1>
+    <router-view />
+  </div>
+</template>
+<style scoped></style>
