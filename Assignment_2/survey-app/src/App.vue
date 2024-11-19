31aa639074ddaad445d4b2e7aea629e745b5f7da
Cesco <saccof@usi.ch>
2024-11-19 09:41:56 +0100

    Added UI and Components

--- Assignment_2/survey-app/src/App.vue
+++ Assignment_2/survey-app/src/App.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  /*import  button example*/
+  import { Button } from "@/components/ui/button";
+  import { Switch } from "@/components/ui/switch";
+  function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+  }
+</script>
 
 <template>
   <div class="container py-8">
     <h1 class="text-2xl font-bold mb-4">Survey App</h1>
-    <router-view />
+    <Button variant="destructive">Click me</Button>
+    <Button variant="outline">Click me</Button>
+    <Button class="text">Click me</Button>
+    <Button @click="toggleDarkMode">IamDarkMode</Button>
   </div>
 </template>
 <style scoped></style>
